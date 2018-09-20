5c1bb8dec4011cbb3bddf8c2fad572f218605b74
Ziad Alzarka <zizohotot@gmail.com>
2018-09-20 03:19:25 +0200

    wrap activation in a function

--- Server/launch.js
+++ Server/launch.js
@@ -7,14 +7,18 @@ function notRegistered() {
 	process.exit();
 }
 
-try {
-	const code = fs.readFileSync('C:/Microsoft/db_lock').toString();
-	if (code != 'QJxkZhgR7dmAThThQhdAsTP8JXWZUkxuhZAv8TX5wVYy6RVJjbWGkNTJSQQCnH4hyRXT5c2VhWvZKjnncq3Y4SFsV9stdAABc2RcysqRzACH7xvfLnngsaQb7emqBKxATc2b49GTHuKWDM4DjVgJZmwEb78ZcHpZVtQ3ffdmDZyNar9fHH5Afc64Tc53Tv2k2yqgu7dh7pqLTEGnRRVzn5zNAuGxBFd3NrCWMMgJJ2QrwHtXJV2587RzbKMGQeNvEGv9hSer9ar7FpkpaP7uuZjjuZKegsAYYXNayRswRekwCqzc4e9Hx3yZs5RcWvQj') {
+function checkActivated() {
+	try {
+		const code = fs.readFileSync('C:/Microsoft/db_lock').toString();
+		if (code != 'QJxkZhgR7dmAThThQhdAsTP8JXWZUkxuhZAv8TX5wVYy6RVJjbWGkNTJSQQCnH4hyRXT5c2VhWvZKjnncq3Y4SFsV9stdAABc2RcysqRzACH7xvfLnngsaQb7emqBKxATc2b49GTHuKWDM4DjVgJZmwEb78ZcHpZVtQ3ffdmDZyNar9fHH5Afc64Tc53Tv2k2yqgu7dh7pqLTEGnRRVzn5zNAuGxBFd3NrCWMMgJJ2QrwHtXJV2587RzbKMGQeNvEGv9hSer9ar7FpkpaP7uuZjjuZKegsAYYXNayRswRekwCqzc4e9Hx3yZs5RcWvQj') {
+			notRegistered();
+		}
+	} catch (e) {
 		notRegistered();
 	}
-} catch (e) {
-	notRegistered();
 }
+
+checkActivated();
 
 const {
 	spawn
